60184e1ff593083405ccc40c0ce870dc9aaae338
Esteban <angelesteba0326@gmail.com>
2022-04-28 18:35:02 -0500

    arregle el reto1 unal porque me quedaron unos valores mal

--- practicandoPython/retos_unal/Reto_1:ciclo-1_grupo-1_programacion.py
+++ practicandoPython/retos_unal/Reto_1:ciclo-1_grupo-1_programacion.py
@@ -2,16 +2,16 @@ def run():
 #calcular las etapas 
 
     pesoChavo = int(input("escribe el peso del Chavo: "))
-    pesoQuico = pesoChavo * 2
+    pesoQuico = (pesoChavo * 2) + 4
     pesoÑoño = int((pesoChavo + pesoQuico) / 5)
 
     print(pesoChavo, pesoQuico, pesoÑoño)
 
     if pesoÑoño <= 20:
         print("uno")
-    elif pesoÑoño >= 21 and pesoÑoño <= 60:
+    elif pesoÑoño >= 21 and pesoÑoño <= 31:
         print("dos")
-    elif pesoÑoño >= 61 and pesoÑoño <= 80:
+    elif pesoÑoño >= 31 and pesoÑoño < 80:
         print("tres")
     else:
         print("cuatro")
